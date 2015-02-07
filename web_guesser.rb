b44283de2876bd93280ac148f5a3abe7559dd7c6
Teresa Finn <teresa.finn@gmail.com>
2015-02-06 21:30:31 -0700

    I3: not working check_guess method

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,22 @@ require 'sinatra/reloader'
 secret_num ||= rand(100)
 
 get '/' do
-  erb :index, :locals => {:secret_num => secret_num}
+  guess = params['guess'].to_i
+  message = check_guess(guess)
+
+  erb :index, :locals => {:secret_num => secret_num, :message => message }
+end
+
+check_guess(guess)
+  if guess > (secret_num + 5)
+      message = "way too high"
+  elsif guess > secret_num
+      message = "too high"
+  elsif guess < (secret_num - 5)
+      message = "way too low"
+  elsif guess < secret_num
+      message = "too low"
+  elsif guess == secret_num
+      message = "CORRECT! The secret number is #{secret_num}"
+  end
 end
